3f6b03d19d131a1592083238169b0fd457b535cd
PHUOC SANG <sangphanphuoc2301@gmail.com>
2023-07-24 21:46:30 +0700

    static files

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,17 +3,17 @@ const router = express.Router();
 
 const userController = require('../controllers/userController');
 
+router.param('id', userController.checkID);
+
 router
     .route('/')
     .get(userController.getAllUsers)
     .post(userController.createUser)
-
 
 router
     .route('/:id')
     .get(userController.getUser)
     .patch(userController.updateUser)
     .delete(userController.deleteUser)
-
 
 module.exports = router;
