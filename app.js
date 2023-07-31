bac9002199ef5cdc4555632874f8e9804a3b0d6c
PHUOC SANG <sangphanphuoc2301@gmail.com>
2023-07-31 21:59:00 +0700

    environment variable

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 // MIDDLEWARE
-app.use(morgan('dev'));
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));   // because when we open up a URL that it can't find in any of our routes, it will then look in that public folder that we defined. And it sets that folder to the root.
 
--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config({ path : './config.env' });
+
 const app = require('./app');
 
+// console.log(process.env);
+
+const port = process.env.PORT;
 app.listen(9000, () => {
-    console.log('App listening on port 9000...');
+    console.log(`App listening on port ${port}...`);
 })
