f3e7dd68b46b17ffd6b6cf375a69d132922586ad
PHUOC SANG <sangphanphuoc2301@gmail.com>
2023-07-31 22:46:44 +0700

    connect to DB

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path : './config.env' });
 
 const app = require('./app');
 
 // console.log(process.env);
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose.connect(DB)
+    .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT;
 app.listen(9000, () => {
