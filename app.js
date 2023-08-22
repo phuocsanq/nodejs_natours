cedb34b579ff645dc24e606c249921d9dadd7d48
PHUOC SANG <sangphanphuoc2301@gmail.com>
2023-08-22 15:55:30 +0700

    catching uncaught exceptions

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));   // because when we open up a URL that it can't find in any of our routes, it will then look in that public folder that we defined. And it sets that folder to the root.
 
 app.use((req, res, next) => {                           // my middleware
-    console.log('Hello from the middleware!')
+    console.log('Hello from the middleware!');
     next();
 })
 app.use((req, res, next) => {                           // my middleware
--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path : './config.env' });
 
+process.on('uncaughtException', err => {
+    console.log(err.name, err.message);
+    console.log('UNCAUGHT EXCEPTION 💥');
+    process.exit(1);
+})
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
@@ -15,7 +20,8 @@ const server = app.listen(port, () => {
 
 process.on('unhandledRejection', err => {
     console.log(err.name, err.message);
+    console.log('UNHANDLED REJECTION 💥');
     server.close(() => {
         process.exit(1);
     })
-})
+});
