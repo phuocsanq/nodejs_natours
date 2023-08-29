b8dfa041436274c2e04de6fa5bb8829555fbbef3
PHUOC SANG <sangphanphuoc2301@gmail.com>
2023-08-29 21:42:52 +0700

    sending JWT via cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,6 +12,29 @@ const signToken = id => {
         expiresIn: process.env.JWT_EXPIRES_IN
     });
 }
+
+const createSendToken = (user, statusCode, res) => {
+    const token = signToken(user._id);
+    
+    const cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        httpOnly: true                     // cannot be accessed or modified in any way by the browser.
+    };
+
+    if(process.env.NODE_ENV === 'production') cookieOptions.secure = true; // secure: true, the cookie would not be created and not be sent to the client.
+    
+    res.cookie('jwt', token, cookieOptions);  
+
+    user.password = undefined;
+
+    res.status(statusCode).json({
+        status: 'success',
+        token,
+        data: {
+            user
+        }
+    })
+}
 exports.signup = catchAsync(async (req, res, next) => {
     const newUser = await User.create(req.body);
     // const newUser = await User.create({
@@ -21,15 +44,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     //     passwordConfim: req.body.passwordConfim
     // });
 
-    const token = signToken(newUser._id);
-
-    res.status(201).json({
-        status: 'success',
-        token,
-        data: {
-            user: newUser
-        }
-    })
+    createSendToken(newUser, 201, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -45,12 +60,7 @@ exports.login = catchAsync(async (req, res, next) => {
         return next(new AppError('Incorrect email or password', 401));
     }
     
-    const token = signToken(user._id);
-
-    res.status(200).json({
-        status: 'success',
-        token
-    })
+    createSendToken(user, 200, res);
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
@@ -145,11 +155,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
         
     // 3) update passwordChangedAt property for the user
     // 4) log the user in, send JWT
-    const token = signToken(user._id);
-    res.status(200).json({
-        status: 'success',
-        token
-    })
+    createSendToken(user, 200, res);
 });
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
@@ -165,9 +171,5 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
     await user.save();
     // await user.findByIdAndUpdate will not work with midleware and validator
 
-    const token = signToken(user._id);
-    res.status(200).json({
-        status: 'success',
-        token
-    })
+    createSendToken(user, 200, res);
 });
