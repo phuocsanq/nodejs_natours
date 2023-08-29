2508f3373129e7eda7aa1de10eea53e0662c9597
PHUOC SANG <sangphanphuoc2301@gmail.com>
2023-08-29 18:30:43 +0700

    deleting current user

--- controllers/userController.js
+++ controllers/userController.js
@@ -50,7 +50,15 @@ exports.updateMe = catchAsync(async (req, res, next) => {
             updatedUser
         }
     })
-    
+});
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+    await User.findByIdAndUpdate(req.user.id, { active: false });
+
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
 });
 
 
--- models/userModel.js
+++ models/userModel.js
@@ -40,7 +40,12 @@ const userSchema = new mongoose.Schema({
     },
     passwordChangedAt: Date,
     passwordResetToken: String,
-    passwordResetExpires: Date
+    passwordResetExpires: Date,
+    active: {
+        type: Boolean,
+        default: true,
+        select: false
+    }
 });
 
 userSchema.pre('save', function(next) {
@@ -56,6 +61,11 @@ userSchema.pre('save', async function(next) {
     this.password = await bcrypt.hash(this.password, 12);
 
     this.passwordConfirm = undefined;
+});
+
+userSchema.pre(/^find/, function(next) {
+    this.find({ active: { $ne: false }});
+    next();
 });
 
 userSchema.methods.isCorrectPassword = async function(cadidatePassword, userPassword) {
