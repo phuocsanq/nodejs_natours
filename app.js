376c9d7b39256a788c385da44bf3ba27691d2de8
PHUOC SANG <sangphanphuoc2301@gmail.com>
2023-09-08 22:23:51 +0700

    including a map with openstreetmap and leaflet

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+const path = require('path');
 const hpp = require('hpp');
 const mongoSanitize= require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const helmet = require('helmet');
+const csp = require('helmet-csp');
+const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 const express = require('express');
 const app = express();
@@ -12,10 +15,59 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// serving static file
+app.use(express.static(path.join(__dirname, 'public')));   // because when we open up a URL that it can't find in any of our routes, it will then look in that public folder that we defined. And it sets that folder to the root.
+
 
 // GLOBAL MIDDLEWARE
 // set security http headers
-app.use(helmet());
+// app.use(helmet({ crossOriginEmbedderPolicy: false, originAgentCluster: true }));
+// app.use(
+//     helmet.contentSecurityPolicy({
+//       useDefaults: true,
+//       directives: {
+//         "img-src": ["'self'", "https: data: blob:"],
+//       },
+//     })
+//   );
+// Sử dụng middleware helmet-csp để cấu hình CSP
+// app.use(
+//     csp({
+//       directives: {
+//         defaultSrc: ["'self'"],
+//         scriptSrc: ["'self'", "'unsafe-inline'", 'example.com'],
+//         // Thêm các directive khác theo cần thiết
+//       },
+//     })
+//   );
+// app.use(
+//     csp({
+//       directives: {
+//         defaultSrc: ["'self'"],
+//         scriptSrc: ["'self'", "'unsafe-inline'", 'https://example.com', "'unsafe-hashes'"],
+//         // Thêm directive 'img-src' để cho phép tải hình ảnh từ 'https://tiles.stadiamaps.com'
+//         // imgSrc: ["'self'", 'https://tiles.stadiamaps.com'],
+//         "img-src": ["'self'", "https: data: blob:", "https://tiles.stadiamaps.com"],
+//         // Thêm các directive khác theo nhu cầu
+//         styleSrc: ["'self'", "'unsafe-inline'", "'unsafe-hashes'"], // Thêm 'unsafe-inline' cho style
+//         fontSrc: ["'self'", 'https://fonts.gstatic.com'], // Thêm nguồn cho font
+//       },
+//     })
+//   );
+
+//   const corsOptions = {
+//     origin: 'https://tiles.stadiamaps.com', // Cho phép truy cập từ trang web cụ thể
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Các phương thức HTTP được cho phép
+//     credentials: true, // Cho phép gửi cookie và thông tin xác thực
+//   };
+// // Sử dụng middleware CORS
+// app.use(cors(corsOptions));
+
 
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
@@ -43,9 +95,6 @@ app.use(hpp({
     whitelist: ['duration', 'maxGroupSize', 'difficulty', 'ratingsAverage', 'ratingsQuantity', 'price']
 }));
 
-// serving static file
-app.use(express.static(`${__dirname}/public`));   // because when we open up a URL that it can't find in any of our routes, it will then look in that public folder that we defined. And it sets that folder to the root.
-
 // test middleware
 app.use((req, res, next) => {                           
     console.log('Hello from the middleware!');
@@ -57,7 +106,10 @@ app.use((req, res, next) => {
     next();
 })
 
+
 // ROUTES
+
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
