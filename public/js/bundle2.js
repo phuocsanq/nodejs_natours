ba91bc82eccc9f3b9097518e57c2bedf2635177e
PHUOC SANG <sangphanphuoc2301@gmail.com>
2024-06-26 03:58:38 +0700

    websocket2

--- public/js/bundle2.js
+++ public/js/bundle2.js
@@ -13928,7 +13928,8 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var port = location.protocol === 'https:' ? location.port: 25596; 
+  var port = location.protocol === 'https:' ? 443 : 25596; 
+  
   // var ws = new WebSocket(protocol + '://' + hostname + ':' + "25596" + '/');
   var ws = new WebSocket(protocol + '://' + hostname + ':' + port + '/');
   ws.onmessage = function (event) {
